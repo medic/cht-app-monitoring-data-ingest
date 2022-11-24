4972fa9b8421e10809f5e5a97e8c3056d581e16b
Elijah Gichinga <elijah@medic.org>
2022-11-24 14:26:48 +0300

    Migrate

---
+++ app-monitoring-data-ingest/matviews/README.md
@@ -0,0 +1,3 @@
+### Refreshing Materialized Views
+
+For the app monitoring material views, the refresh functions only refreshes matviews that follow the `app_monitoring{YOUR MATVIEW NAME}` naming convention
---
+++ app-monitoring-data-ingest/postgres/dashboard/klipfolio_datasource_refresh.sql
@@ -0,0 +1,30 @@
+DROP MATERIALIZED VIEW IF EXISTS app_monitoring_klipfolio_datasource_refresh;
+
+CREATE MATERIALIZED VIEW app_monitoring_klipfolio_datasource_refresh AS
+  WITH refresh_age_CTE AS (
+    SELECT
+      id,
+      url_id,
+      created,
+      jsonb_array_length(doc) AS total_datasources,
+      (jsonb_array_elements(doc)::json ->> 'refresh_interval')::INT AS refresh_interval,
+      EXTRACT(epoch FROM AGE(created, (jsonb_array_elements(doc)::json ->> 'date_last_refresh')::TIMESTAMP)) AS refresh_age
+    FROM monitoring_docs
+    WHERE doctype='klipfolio_datasource'
+  )
+  SELECT
+    docs.id,
+    created,
+    partner_name,
+    total_datasources,
+    COUNT(*) FILTER ( WHERE refresh_age <= refresh_interval ) AS successful_refresh,
+    COUNT(*) FILTER ( WHERE refresh_age > refresh_interval AND refresh_age <= refresh_interval * 5 ) AS potential_failed_refresh,
+    COUNT(*) FILTER ( WHERE refresh_age > refresh_interval * 5 ) AS failed_refresh,
+    COUNT(*) FILTER ( WHERE refresh_age > refresh_interval )/total_datasources AS percent_failed_refresh
+  FROM refresh_age_CTE AS docs
+  LEFT JOIN monitoring_urls AS partners ON (partners.id = docs.url_id)
+  GROUP by docs.id, created, total_datasources, partner_name;
+
+CREATE UNIQUE INDEX IF NOT EXISTS app_monitoring_klipfolio_datasource_refresh_id_created_partner_name ON app_monitoring_klipfolio_datasource_refresh USING btree(id, created, partner_name);
+
+GRANT SELECT ON app_monitoring_klipfolio_datasource_refresh TO superset;
