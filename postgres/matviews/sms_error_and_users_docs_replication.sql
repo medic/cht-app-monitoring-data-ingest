f77d045c2ba1a7a9f7f763737874adfc9eb18eb6
jdndiaye <justinho902013@gmail.com>
2023-02-28 12:08:04 +0000

    using lag function

--- postgres/matviews/sms_error_and_users_docs_replication.sql
+++ postgres/matviews/sms_error_and_users_docs_replication.sql
@@ -63,40 +63,22 @@ SELECT COALESCE(mu.partner_name, mu.url) AS partner_name,
         ) AS md
         JOIN monitoring_urls AS mu ON md.url_id = mu.id
     WHERE md.rank = 1
-), delta_of_docs AS (
-    SELECT c1.url_id,
-        c1.reported,
-        "users past replication_limit",
-        c1.doc_count_today - c2.doc_count_yesterday AS dalta_of_doc
-    FROM (
-        SELECT
-            url_id,
-            TO_DATE(created::text, 'YYYY-MM-DD') as reported,
-            COALESCE((doc #>> '{replication_limit, count}')::int, 0) AS "users past replication_limit",
-            COALESCE((doc #>> '{couchdb,medic,doc_count}')::int, 0) AS doc_count_today
-        FROM monitoring_docs
-        WHERE doctype = 'monitoring') AS c1
-    JOIN (
-        SELECT url_id,
-            TO_DATE(created::text, 'YYYY-MM-DD') as reported,
-            COALESCE((doc #>> '{couchdb,medic,doc_count}')::int, 0) AS doc_count_yesterday
-        FROM monitoring_docs
-        WHERE doctype = 'monitoring') AS c2
-    ON c1.url_id = c2.url_id and c1.reported = c2.reported + 1
 )
   SELECT
     sms.partner_name,
-    docs.reported,
+    sms.reported,
     sms.due,
     sms.muted,
     sms.failed,
     sms.delivered,
     sms.scheduled,
-    "users past replication_limit",
-    dalta_of_doc
-  FROM delta_of_docs AS docs
+    COALESCE((doc #>> '{replication_limit, count}')::int, 0) AS "users past replication_limit",
+    COALESCE((doc #>> '{couchdb,medic,doc_count}')::int, 0) - COALESCE(lag((doc #>> '{couchdb,medic,doc_count}')::int)
+        OVER (PARTITION BY docs.url_id ORDER BY created), 0) AS dalta_of_doc
+  FROM monitoring_docs AS docs
   INNER JOIN monitoring_urls AS urls ON (docs.url_id=urls.id)
-  JOIN sms_error_states AS sms ON urls.partner_name = sms.partner_name AND docs.reported = sms.reported
+  JOIN sms_error_states AS sms ON urls.partner_name = sms.partner_name AND TO_DATE(created::text, 'YYYY-MM-DD') = sms.reported
+  WHERE doctype = 'monitoring'
 WITH DATA;
 
 GRANT SELECT ON app_monitoring_sms_error_and_users_docs_replication to superset;
