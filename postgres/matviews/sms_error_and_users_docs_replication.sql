f5a90d07bf056daee2639b4d379e7066808a3686
Justin Diomaye NDIAYE <77727556+jdndiaye@users.noreply.github.com>
2023-03-01 22:20:43 +0000

    replication metrics (#48)
    Merged and deployed to postgres

---
+++ postgres/matviews/sms_error_and_users_docs_replication.sql
@@ -0,0 +1,28 @@
+/* In this view we regroup the metrics of:
+  - sms_error_states
+  - users_replication_limit
+  - replication docs
+*/
+CREATE MATERIALIZED VIEW public.app_monitoring_sms_error_and_users_docs_replication
+TABLESPACE pg_default
+AS
+SELECT DISTINCT ON (COALESCE(partner_name, urls.url),
+    TO_DATE(created::text, 'YYYY-MM-DD')) partner_name,
+    TO_DATE(created::text, 'YYYY-MM-DD') AS reported,
+    COALESCE((doc #>>'{messaging,outgoing,state,due}')::int,0)as due,
+    COALESCE((doc #>>'{messaging,outgoing,state,muted}')::int,0)as muted,
+    COALESCE((doc #>>'{messaging,outgoing,state,failed}')::int,0)as failed,
+    COALESCE((doc #>>'{messaging,outgoing,state,delivered}')::int,0)as delivered,
+    COALESCE((doc #>>'{messaging,outgoing,state,scheduled}')::int,0) as scheduled,
+    COALESCE((doc #>> '{replication_limit, count}')::int, 0) AS "users past replication_limit",
+    COALESCE((doc #>> '{couchdb,medic,doc_count}')::int, 0) - COALESCE(lag((doc #>> '{couchdb,medic,doc_count}')::int)
+        OVER (PARTITION BY docs.url_id ORDER BY created), 0) AS delta_of_doc
+FROM monitoring_docs AS docs
+INNER JOIN monitoring_urls AS urls ON (docs.url_id=urls.id)
+WHERE doctype = 'monitoring'
+WITH DATA;
+
+GRANT SELECT ON app_monitoring_sms_error_and_users_docs_replication to superset;
+
+-- View indexes:
+CREATE UNIQUE INDEX app_monitoring_sms_error_and_users_docs_replication_index ON public.app_monitoring_sms_error_and_users_docs_replication USING btree(partner_name, reported);
