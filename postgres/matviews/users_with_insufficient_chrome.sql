c35d6455f7a3ae7dd50b3b631f0330d97439730b
Kenn Sippell <kennsippell@gmail.com>
2023-04-10 11:19:16 -0700

    Table for users with insufficient Chrome version (#71)
    #70 
    
    ![Screenshot from 2023-03-31
    01-13-50](https://user-images.githubusercontent.com/9014751/229064881-d261e084-1c8e-448f-aa8c-4a8e97bd5c52.png)

---
+++ postgres/matviews/users_with_insufficient_chrome.sql
@@ -0,0 +1,12 @@
+DROP MATERIALIZED VIEW if EXISTS app_monitoring_users_with_insufficient_chrome;
+CREATE MATERIALIZED VIEW app_monitoring_users_with_insufficient_chrome AS (
+    SELECT
+        partner AS partner_name,
+        user_name,
+        chrome_version,
+        required_chrome_version
+    FROM
+        get_users_with_insufficient_chrome()
+);
+
+GRANT SELECT ON app_monitoring_users_with_insufficient_chrome TO superset;
