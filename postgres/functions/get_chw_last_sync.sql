91e758ee5b764500b156d4a5d314032204fd6fb1
freddieptf <muiru@medic.org>
2023-05-12 20:49:43 +0300

    update engagement query

--- postgres/functions/get_chw_last_sync.sql
+++ postgres/functions/get_chw_last_sync.sql
@@ -25,13 +25,13 @@ BEGIN
                 current_database() as partner,
                 form.chw as chw_id,
                 c_user.doc->>''name'' as chw_username,
-                max(c_info.doc->>''latest_replication_date'') as last_sync
+                max(c_info.doc->>''latest_replication_date'')::date as last_sync
             FROM
                 form_metadata form
             JOIN couchdb c_info ON form.uuid=c_info.doc->>''doc_id''
             JOIN couchdb c_user ON form.chw=c_user.doc->>''contact_id''
             WHERE
-                c_info.doc->>''type'' = ''info'' and c_user.doc->>''type''=''user-settings''
+                c_info.doc->>''type'' = ''info'' and c_info.doc->>''latest_replication_date'' != ''unknown'' and c_user.doc->>''type''=''user-settings''
             GROUP BY
                 form.chw, c_user.doc->>''name'';
             ',
--- postgres/matviews/chw_engagement.sql
+++ postgres/matviews/chw_engagement.sql
@@ -1,16 +1,11 @@
 CREATE MATERIALIZED VIEW public.app_monitoring_chw_engagement
 TABLESPACE pg_default
 AS
-  SELECT
-    fn.partner AS partner_name,
-    fn.chw_id,
-    fn.chw_username,
-    fn.last_sync
-  FROM get_chw_last_sync() AS fn(partner, chw_id, chw_username, last_sync)
-  INNER JOIN impactconfig AS ic ON fn.partner = ic.partner_name
+  SELECT * FROM app_monitoring_chw_last_sync WHERE last_sync BETWEEN now() - '60 days'::interval AND now() - '30 days'::interval
+  EXCEPT
+  SELECT * FROM app_monitoring_chw_last_sync WHERE last_sync > now() - '30 days'::interval;
 WITH DATA;
 
 GRANT SELECT ON app_monitoring_chw_engagement TO superset;
-
 -- View indexes:
 CREATE UNIQUE INDEX app_monitoring_chw_engagement_partner_chw ON public.app_monitoring_chw_engagement USING btree(partner_name, chw_id);
