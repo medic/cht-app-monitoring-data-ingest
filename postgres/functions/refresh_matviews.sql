9c729381c4eb1f07a9bf652dd683e5eaecc8ffaf
freddieptf <muiru@medic.org>
2023-06-02 13:18:23 +0300

    use clock_timestamp() for start time

--- postgres/functions/refresh_matviews.sql
+++ postgres/functions/refresh_matviews.sql
@@ -6,7 +6,7 @@ DECLARE
   matview RECORD;
   start_time timestamp with time zone;
 BEGIN
-  start_time := transaction_timestamp();
+  start_time := clock_timestamp();
   FOR matview IN SELECT matviewname FROM pg_catalog.pg_matviews where matviewname like 'app_monitoring%' LOOP
     RAISE NOTICE 'Refreshing %', matview.matviewname;
     EXECUTE format('REFRESH MATERIALIZED VIEW %I', matview.matviewname);
