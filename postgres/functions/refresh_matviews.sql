fd5fa1d36335be9d60c73011a9005aac182246f1
freddieptf <muiru@medic.org>
2023-06-02 13:32:55 +0300

    init start_time in the loop

--- postgres/functions/refresh_matviews.sql
+++ postgres/functions/refresh_matviews.sql
@@ -6,9 +6,9 @@ DECLARE
   matview RECORD;
   start_time timestamp with time zone;
 BEGIN
-  start_time := clock_timestamp();
   FOR matview IN SELECT matviewname FROM pg_catalog.pg_matviews where matviewname like 'app_monitoring%' LOOP
     RAISE NOTICE 'Refreshing %', matview.matviewname;
+    start_time := clock_timestamp();
     EXECUTE format('REFRESH MATERIALIZED VIEW %I', matview.matviewname);
     EXECUTE format('INSERT INTO matviews_log(view_name, create_or_refresh, start_date, elapsed_time) VALUES (''%I'', ''R'', now(), ''%s'');', matview.matviewname, clock_timestamp() - start_time);
   END LOOP;
