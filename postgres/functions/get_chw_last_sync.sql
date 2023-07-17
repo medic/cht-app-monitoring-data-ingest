ad429954157468480c91d4addc40fb78bf897012
freddieptf <muiru@medic.org>
2023-07-18 00:07:05 +0300

    edut sync query

--- postgres/functions/get_chw_last_sync.sql
+++ postgres/functions/get_chw_last_sync.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION public.get_chw_last_sync() RETURNS TABLE(partner text, chw_id text, chw_username text, last_sync date) AS $$
+CREATE OR REPLACE FUNCTION public.get_chw_last_sync() RETURNS TABLE(partner text, chw_id text, chw_username text, replication_date text) AS $$
 DECLARE partners cursor IS (
         SELECT DISTINCT ON (partner_name)
             partner_name AS name,
@@ -9,34 +9,32 @@ DECLARE partners cursor IS (
 DECLARE credentials record;
 BEGIN 
     SELECT value->>'user' AS user, value->>'password' AS password FROM configuration WHERE KEY = 'dblink' INTO credentials;
-    FOR partner IN partners LOOP RETURN query
-    SELECT
-        *
-    FROM dblink(
-            FORMAT(
-                'dbname=%s host=localhost port=%s user=%s password=%s',
-                partner.name,
-                partner.port,
-                credentials.user,
-                credentials.password
-            ),
+    FOR partner IN partners LOOP
+    -- create connection
+    PERFORM dblink_connect('app_monitoring_idx', FORMAT('dbname=%s host=localhost port=%s user=%s password=%s', partner.name, partner.port, credentials.user, credentials.password));
+    -- create indexes if they don't exist
+    PERFORM dblink_exec('app_monitoring_idx', 'CREATE INDEX IF NOT EXISTS couchdb_doc_id ON couchdb USING BTREE ((doc->>''doc_id''));');
+    PERFORM dblink_exec('app_monitoring_idx', 'CREATE INDEX IF NOT EXISTS couchdb_doc_contact_id ON couchdb USING BTREE ((doc->>''contact_id''));');
+    PERFORM dblink_exec('app_monitoring_idx', 'CREATE INDEX IF NOT EXISTS couchdb_doc_replication_date ON couchdb USING BTREE ((doc->>''latest_replication_date''));');
+    -- disconnect
+    PERFORM dblink_disconnect('app_monitoring_idx');
+    -- return query result
+    RETURN QUERY SELECT * FROM dblink(
+        FORMAT('dbname=%s host=localhost port=%s user=%s password=%s', partner.name, partner.port, credentials.user, credentials.password),
             '
             SELECT
                 current_database() as partner,
                 form.chw as chw_id,
                 c_user.doc->>''name'' as chw_username,
-                max(c_info.doc->>''latest_replication_date'')::date as last_sync
+                c_info.doc->>''latest_replication_date'' as replication_date
             FROM
                 form_metadata form
-            JOIN couchdb c_info ON form.uuid=c_info.doc->>''doc_id''
-            JOIN couchdb c_user ON form.chw=c_user.doc->>''contact_id''
-            WHERE
-                c_info.doc->>''type'' = ''info'' and c_info.doc->>''latest_replication_date'' != ''unknown'' and c_user.doc->>''type''=''user-settings''
-            GROUP BY
-                form.chw, c_user.doc->>''name'';
+            INNER JOIN couchdb c_info ON form.uuid=c_info.doc->>''doc_id''
+            INNER JOIN couchdb c_user ON form.chw=c_user.doc->>''contact_id''
+            WHERE form.reported > now() - ''120 days''::interval;
             ',
             FALSE
-        ) result(partner text, chw_id text, chw_username text, last_sync date);
+        ) result(partner text, chw_id text, chw_username text, replication_date text);
 END LOOP;
 END;
 $$ language plpgsql;
--- postgres/matviews/chw_last_sync.sql
+++ postgres/matviews/chw_last_sync.sql
@@ -1,14 +1,13 @@
 CREATE MATERIALIZED VIEW public.app_monitoring_chw_last_sync
 TABLESPACE pg_default
-AS
-  SELECT
-    fn.partner AS partner_name,
+AS SELECT fn.partner AS partner_name,
     fn.chw_id,
     fn.chw_username,
-    fn.last_sync,
-    EXTRACT(DAY FROM now() - fn.last_sync) AS days_since_last_sync
-  FROM get_chw_last_sync() AS fn(partner, chw_id, chw_username, last_sync)
-  INNER JOIN impactconfig AS ic ON fn.partner = ic.partner_name
+    fn.replication_date::date AS last_sync,
+    date_part('day'::text, now() - fn.replication_date::date::timestamp with time zone) AS days_since_last_sync
+   FROM get_chw_last_sync() fn(partner, chw_id, chw_username, replication_date)
+     JOIN impactconfig ic ON fn.partner = ic.partner_name
+  WHERE fn.replication_date <> 'unknown'::text
 WITH DATA;
 
 GRANT SELECT ON app_monitoring_chw_last_sync TO superset;
