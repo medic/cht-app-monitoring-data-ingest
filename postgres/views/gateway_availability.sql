7f195238dcc8c4e19fdf9e2725f53fbfb0104871
freddieptf <muiru@medic.org>
2023-07-22 02:06:25 +0300

    make sure subquery is looking at the same project

--- postgres/views/gateway_availability.sql
+++ postgres/views/gateway_availability.sql
@@ -2,19 +2,28 @@ CREATE OR REPLACE VIEW app_monitoring_gateway_health AS (
     SELECT
         metrics.*,
         CASE
-            WHEN metrics.due > 0 AND metrics.delivered = 0 THEN 'Gateway Unhealthy'
+            WHEN
+                metrics.due > 0 AND metrics.delivered = 0 THEN 'Gateway Unhealthy'
             ELSE
                 'Gateway Healthy'
         END AS gateway_state,
         CASE
-            WHEN metrics.due > 0 AND metrics.delivered = 0 THEN 
-                (SELECT a.reported FROM get_last_sms_metric() a WHERE a.reported < metrics.reported AND a.delivered > 0 LIMIT 1)
-            ELSE 
+            WHEN metrics.due > 0 AND metrics.delivered = 0 THEN
+                (
+                    SELECT
+                        a.reported
+                    FROM
+                        get_last_sms_metric() AS a
+                    WHERE
+                        a.reported < metrics.reported AND a.delivered > 0 AND a.partner_name = metrics.partner_name
+                    LIMIT 1
+                )
+            ELSE
                 metrics.reported
         END AS last_delivered
     FROM
-        get_last_sms_metric() metrics
+        get_last_sms_metric() AS metrics
 );
 
-GRANT SELECT ON app_monitoring_gateway_health TO superset; 
+GRANT SELECT ON app_monitoring_gateway_health TO superset;
 ALTER VIEW public.app_monitoring_gateway_health OWNER TO full_access;
